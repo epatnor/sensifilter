22de045e52c653e83ca08223cc0d6d41999227e9
Patrik Nordlund <130709148+epatnor@users.noreply.github.com>
2025-07-06 15:53:43 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,3 +1,21 @@
 # sensifilter/utils.py
 
 # Common helper functions (e.g., resize, image loader)
+# utils.py
+
+from PIL import Image
+
+
+def load_image(image_path):
+    """
+    Loads an image using PIL and converts to RGB.
+    """
+    with Image.open(image_path) as img:
+        return img.convert("RGB")
+
+
+def resize_image(image, size=(224, 224)):
+    """
+    Resizes an image to a given size.
+    """
+    return image.resize(size)
