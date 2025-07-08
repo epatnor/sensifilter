0368920ae3b9bcd1c7e0ee648cbcbfc958f51563
Patrik Nordlund <130709148+epatnor@users.noreply.github.com>
2025-07-08 12:35:34 +0200

    Update utils.py

--- sensifilter/utils.py
+++ sensifilter/utils.py
@@ -1,7 +1,6 @@
 # utils.py
 
 import cv2
-import numpy as np
 from PIL import Image
 
 
@@ -22,25 +21,3 @@ def load_image_bgr(image_path):
 # Skalar en PIL-bild till angiven storlek (används av vissa modeller)
 def resize_image(image, size=(224, 224)):
     return image.resize(size)
-
-
-# Beräknar uppskattad andel hud i en bild baserat på RGB-tröskelregler
-def estimate_skin_percent(image_path):
-    image = load_image(image_path)
-    pixels = list(image.getdata())
-    skin_pixels = 0
-
-    for pixel in pixels:
-        r, g, b = pixel
-        if (
-            r > 95 and g > 40 and b > 20 and
-            max(pixel) - min(pixel) > 15 and
-            abs(r - g) > 15 and r > g and r > b
-        ):
-            skin_pixels += 1
-
-    total_pixels = len(pixels)
-    if total_pixels == 0:
-        return 0.0
-
-    return (skin_pixels / total_pixels) * 100
